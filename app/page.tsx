aab18bde1d00da1c71b82eccb091784d709e145a
srv-cashpay <aseprayana@outlook.com>
2025-04-15 02:10:17 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 
       {/* Subtitle + Android App Button */}
-      <div className="my-16 text-center animate-fade-in flex flex-col items-center gap-4">
+      <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500">
         Simplifying payments, one transaction at a time.
         </h2>
