de060a3515080d49ba4828c13984fdb26517b283
srv-cashpay <aseprayana@outlook.com>
2025-04-16 03:08:22 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 
       {/* Subtitle + Android App Button */}
-      <div className="my-16 text-center animate-fade-in flex flex-col items-center gap-4">
+      <div className="my-16 text-center animate-fade-in">
 
         {/* Android App Google Play Badge */}
         <Link
