4067583cf5aa4aed2c4c1d116b08eb72160febba
Asep rayana <asep.rayana09@gmail.com>
2025-04-15 15:52:55 +0700

    penyesuaian

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,15 @@ import Particles from "./components/particles";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+      <nav className="my-16 animate-fade-in">
+          <h2 className="text-sm duration-500 text-zinc-500 hover:text-zinc-300">
+            Simplifying payments, one transaction at a time.
+            </h2>
+      </nav>
 
       {/* Top Glow */}
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <h2 className="text-sm text-zinc-500">
-        Simplifying payments, one transaction at a time.
-        </h2>
+      
 
       {/* Particles */}
       <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100} />
