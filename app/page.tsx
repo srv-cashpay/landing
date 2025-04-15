6846d5dbed4f05ea1ae18461790f2af4cf4bc519
srv-cashpay <aseprayana@outlook.com>
2025-04-16 03:13:50 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import Image from "next/image";
 import React from "react";
 import Particles from "./components/particles";
+
 const navigation = [
   { name: "Projects", href: "/projects" },
   { name: "Contact", href: "/contact" },
@@ -23,36 +23,29 @@ export default function Home() {
           ))}
         </ul>
       </nav>
-
-      {/* Top Glow */}
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      
-
-      {/* Particles */}
-      <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100} />
-
-      {/* Title */}
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
-        cashpay
+      <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={100}
+      />
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+        chronark
       </h1>
 
-      {/* Bottom Glow */}
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-
-      {/* Subtitle + Android App Button */}
       <div className="my-16 text-center animate-fade-in">
-
-        {/* Android App Google Play Badge */}
-        <Link
-          href="https://play.google.com/store/apps/details?id=com.your.package" // Ganti dengan link asli
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/google-play-app-store.png" // Ganti sesuai file di folder /public
-            alt="Get it on Google Play"          />
-        </Link>
+        <h2 className="text-sm text-zinc-500 ">
+          I'm building{" "}
+          <Link
+            target="_blank"
+            href="https://unkey.dev"
+            className="underline duration-500 hover:text-zinc-300"
+          >
+            unkey.dev
+          </Link> to solve API authentication and authorization for developers.
+        </h2>
       </div>
     </div>
   );
+
 }
