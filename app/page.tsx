3b00d5b6d6112cef49729e47067d7c3ee6974831
srv-cashpay <aseprayana@outlook.com>
2025-04-16 03:01:45 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,26 @@ import Link from "next/link";
 import Image from "next/image";
 import React from "react";
 import Particles from "./components/particles";
-
+const navigation = [
+  { name: "Projects", href: "/projects" },
+  { name: "Contact", href: "/contact" },
+];
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <nav className="my-16 animate-fade-in">
-          <h2 className="text-sm duration-500 text-zinc-500 hover:text-zinc-300">
-            Simplifying payments, one transaction at a time.
-            </h2>
+        <ul className="flex items-center justify-center gap-4">
+          {navigation.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+            >
+              {item.name}
+            </Link>
+          ))}
+        </ul>
       </nav>
 
       {/* Top Glow */}
