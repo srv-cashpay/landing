8d05883f73edcd267a96b318b9b82f90cdcf9280
Asep rayana <asep.rayana09@gmail.com>
2025-06-08 06:19:01 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 import Hero from "@/components/Hero";
-import Testimonials from "@/components/Testimonials";
-import Pricing from "@/components/Pricing/Pricing";
-import FAQ from "@/components/FAQ";
 import Logos from "@/components/Logos";
 import Benefits from "@/components/Benefits/Benefits";
 import Container from "@/components/Container";
-import Section from "@/components/Section";
-import Stats from "@/components/Stats";
 import CTA from "@/components/CTA";
 
 const HomePage: React.FC = () => {
